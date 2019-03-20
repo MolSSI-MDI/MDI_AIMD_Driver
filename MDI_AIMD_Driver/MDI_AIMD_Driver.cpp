02c3871be9e2cc99b7631f195c6dc42f4ffdc087
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-03-20 12:32:39 -0400

    Add initial driver code

--- MDI_AIMD_Driver/MDI_AIMD_Driver.cpp
+++ MDI_AIMD_Driver/MDI_AIMD_Driver.cpp
@@ -45,13 +45,94 @@ int main(int argc, char **argv) {
   }
 
   // Connect to the engines
-  // <YOUR CODE GOES HERE>
+  MDI_Comm mm_comm = MDI_NULL_COMM;
+  MDI_Comm qm_comm = MDI_NULL_COMM;
+  int nengines = 2;
+  for (int iengine=0; iengine < nengines; iengine++) {
+    MDI_Comm comm = MDI_Accept_Communicator();
+ 
+    // Determine the name of this engine
+    char* engine_name = new char[MDI_NAME_LENGTH];
+    MDI_Send_Command("<NAME", comm);
+    MDI_Recv(engine_name, MDI_NAME_LENGTH, MDI_CHAR, comm);
+ 
+    cout << "Engine name: " << engine_name << endl;
+ 
+    if ( strcmp(engine_name, "MM") == 0 ) {
+      if ( mm_comm != MDI_NULL_COMM ) {
+	throw string("Accepted a communicator from a second MM engine.");
+      }
+      mm_comm = comm;
+    }
+    else if ( strcmp(engine_name, "QM") == 0 ) {
+      if ( qm_comm != MDI_NULL_COMM ) {
+	throw string("Accepted a communicator from a second QM engine.");
+      }
+      qm_comm = comm;
+    }
+    else {
+      throw string("Unrecognized engine name.");
+    }
+ 
+    delete[] engine_name;
+  }
 
   // Perform the simulation
-  // <YOUR CODE GOES HERE>
+  int niterations = 10;  // Number of MD iterations
+  int natoms;
+  double qm_energy;
+  double mm_energy;
+ 
+  // Receive the number of atoms from the MM engine
+  MDI_Send_Command("<NATOMS", mm_comm);
+  MDI_Recv(&natoms, 1, MDI_INT, mm_comm);
+ 
+  // Allocate the arrays for the coordinates and forces
+  double coords[3*natoms];
+  double forces[3*natoms];
+
+  // Have the MD engine initialize a new MD simulation
+  MDI_Send_Command("MD_INIT", mm_comm);
+ 
+  // Perform each iteration of the simulation
+  for (int iiteration = 0; iiteration < niterations; iiteration++) {
+
+    // Receive the coordinates from the MM engine
+    MDI_Send_Command("<COORDS", mm_comm);
+    MDI_Recv(&coords, 3*natoms, MDI_DOUBLE, mm_comm);
+ 
+    // Send the coordinates to the QM engine
+    MDI_Send_Command(">COORDS", qm_comm);
+    MDI_Send(&coords, 3*natoms, MDI_DOUBLE, qm_comm);
+ 
+    // Have the QM engine perform an SCF calculation
+    MDI_Send_Command("SCF", qm_comm);
+ 
+    // Get the QM energy
+    MDI_Send_Command("<ENERGY", qm_comm);
+    MDI_Recv(&qm_energy, 1, MDI_DOUBLE, qm_comm);
+ 
+    // Get the MM energy
+    MDI_Send_Command("<ENERGY", mm_comm);
+    MDI_Recv(&mm_energy, 1, MDI_DOUBLE, mm_comm);
+ 
+    // Receive the forces from the QM engine
+    MDI_Send_Command("<FORCES", qm_comm);
+    MDI_Recv(&forces, 3*natoms, MDI_DOUBLE, qm_comm);
+ 
+    // Send the forces to the MM engine
+    MDI_Send_Command(">FORCES", mm_comm);
+    MDI_Send(&forces, 3*natoms, MDI_DOUBLE, mm_comm);
+ 
+    // Do an MD timestep
+    MDI_Send_Command("ATOM_STEP", mm_comm);
+ 
+    cout << "timestep: " << iiteration << " " << mm_energy << " " << qm_energy << endl;
+  }
 
   // Send the "EXIT" command to each of the engines
-  // <YOUR CODE GOES HERE>
+  MDI_Send_Command("EXIT", mm_comm);
+  MDI_Send_Command("EXIT", qm_comm);
 
   // Synchronize all MPI ranks
   MPI_Barrier(world_comm);
