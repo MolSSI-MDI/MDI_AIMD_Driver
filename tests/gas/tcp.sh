7a5ef49086703eff36666e3cb3ed3512db1a195c
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-06-18 14:09:02 -0400

    Add gas-phase example

---
+++ tests/gas/tcp.sh
@@ -0,0 +1,26 @@
+#location of required codes
+DRIVER_LOC=$(cat ../locations/MDI_AIMD_Driver)
+LAMMPS_LOC=$(cat ../locations/LAMMPS)
+QE_LOC=$(cat ../locations/QE)
+
+#remove old files
+if [ -d work ]; then 
+  rm -r work
+fi
+
+#create work directory
+cp -r data work
+cd work
+
+#launch Psi4
+(cd /home/tbarnes/mdi/plugin_test/mdi_engine; psi4 -n 16 -i input.dat) &
+#export PYTHONPATH=$PYTHONPATH:/home/tbarnes/mdi/plugin_test/mdi_engine
+#psi4 -n 16 -i /home/tbarnes/mdi/plugin_test/mdi_engine/input.dat &
+
+#launch LAMMPS
+${LAMMPS_LOC} -mdi "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost" -in lammps.in > lammps.out &
+
+#launch driver
+${DRIVER_LOC} -mdi "-role DRIVER -name driver -method TCP -port 8021" &
+
+wait
