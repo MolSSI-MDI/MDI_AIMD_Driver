16adeeba08ea73efbc4adb84c6d94a38315093f1
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-07-16 12:27:44 -0400

    Add -out option for LAMMPS and QE

--- tests/water/mpi.sh
+++ tests/water/mpi.sh
@@ -14,7 +14,7 @@ cd work
 
 #launch the codes
 mpiexec -n 1 ${DRIVER_LOC} -mdi "-role DRIVER -name driver -method MPI -out driver.out" : \
-    -n 1 ${LAMMPS_LOC} -mdi "-role ENGINE -name MM -method MPI" -in lammps.in > lammps.out : \
-    -n 8 ${QE_LOC} -mdi "-role ENGINE -name QM -method MPI" -in qe.in > qe.out
+    -n 1 ${LAMMPS_LOC} -mdi "-role ENGINE -name MM -method MPI -out lammps.out" -in lammps.in : \
+    -n 8 ${QE_LOC} -mdi "-role ENGINE -name QM -method MPI -out qe.out" -in qe.in
 
 wait
