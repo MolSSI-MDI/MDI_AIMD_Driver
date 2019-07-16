dbaf7c2c44e317b947967f3569b19de7e67439ff
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-07-16 11:36:41 -0400

    Add -out option to mpi.sh

--- tests/water/mpi.sh
+++ tests/water/mpi.sh
@@ -13,7 +13,7 @@ cp -r data work
 cd work
 
 #launch the codes
-mpiexec -n 1 ${DRIVER_LOC} -mdi "-role DRIVER -name driver -method MPI" : \
+mpiexec -n 1 ${DRIVER_LOC} -mdi "-role DRIVER -name driver -method MPI -out driver.out" : \
     -n 1 ${LAMMPS_LOC} -mdi "-role ENGINE -name MM -method MPI" -in lammps.in > lammps.out : \
     -n 8 ${QE_LOC} -mdi "-role ENGINE -name QM -method MPI" -in qe.in > qe.out
 
