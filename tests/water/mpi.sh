36657c07f938a7444fbaaccd6e5d46e5c70961e8
taylor-a-barnes <taylor.a.barnes@gmail.com>
2021-03-24 14:15:29 -0400

    Update lammps example

--- tests/water/mpi.sh
+++ tests/water/mpi.sh
@@ -18,6 +18,6 @@ export OMP_NUM_THREADS=1
 #launch the codes
 mpiexec -n 1 ${DRIVER_LOC} -mdi "-role DRIVER -name driver -method MPI -out driver.out" : \
     -n 1 ${LAMMPS_LOC} -mdi "-role ENGINE -name MM -method MPI -out lammps.out" -in lammps.in : \
-    -n 8 ${QE_LOC} -mdi "-role ENGINE -name QM -method MPI -out qe.out" -in qe.in
+    -n 2 ${QE_LOC} -mdi "-role ENGINE -name QM -method MPI -out qe.out" -in qe.in
 
 wait
